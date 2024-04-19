5ce0272e666beac6e2b2cdce7302fd80cdd37963
Dimitris Stavros Vaiopoulos <dim.vaiopoulos@gmail.com>
2024-04-19 14:38:55 +0300

    some styling at the main page of the app, a nav bar and 2 pages for registration and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,60 @@
-
 function App() {
   return (
-    <div className='container'>Hello world</div>
-
+    <>
+      <div className="container text-center mw-100 back-color ">
+        <h1 className="display-1">Welcome to MyChat</h1>
+      </div>
+      <div className="container text-center no-color">
+        <div className="row d-flex justify-content-around flex-wrap">
+          <div className="col-12 col-lg-5 no-color">
+            <h2>Sooooo what are we doing here...</h2>
+            <p className="p">
+              This is a place where you can meet online with your friends and
+              chat!! <br />
+              You just have to register and then you can log in and chat with
+              your friends in real time!!
+            </p>
+            <h3 className="mt-5">Then what??</h3>
+            <p className="p">
+              Then the steps are simple... <br />
+              Find all the users <br />
+              Select the one you want to talk to <br />
+              Start a conversation!
+            </p>
+            <a href="/register" className="btn btn-primary">Register</a>
+          </div>
+          <div className="col-12 col-lg-3 no-color">
+            <h2>Already a user?? </h2>
+            <form>
+              <div className="mb-3">
+                <label for="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  aria-describedby="username"
+                />
+              </div>
+              <div className="mb-3">
+                <label for="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,8 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Dashboard from "./pages/dashboard";
+import Register from "./pages/register";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +28,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<App></App>}></Route>
+      <Route path="dashboard" element={<Dashboard></Dashboard>}></Route>
+      <Route path="register" element={<Register></Register>}></Route>
     </Route>
   )
 );
@@ -33,10 +37,52 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          MyChat
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="container mw-100">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">
+                  Dashboard
+                </a>
+              </li>
+              <li className="nav-item ms-lg-auto">
+                <a className="nav-link bg-info" href="/register">
+                  <b>Register</b>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    <div className="container py-5 text-center mw-100 back-color">
+      <small>Copyright by MyChat</small>
+    </div>
   </React.StrictMode>
 );
 
